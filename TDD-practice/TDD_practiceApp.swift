ed115018960e109ecba8b52bd4bc64544250960a
bumgeun.song <bumgeun.song@toss.im>
2023-08-05 18:30:11 +0900

    Initial Commit

---
+++ TDD-practice/TDD_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDD_practiceApp.swift
+//  TDD-practice
+//
+//  Created by 송범근 on 2023/08/05.
+//
+
+import SwiftUI
+
+@main
+struct TDD_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
