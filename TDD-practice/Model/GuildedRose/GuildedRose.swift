e5c1f9e62aedc83cf2eefeb4269554cb516acede
bumgeun.song <bumgeun.song@toss.im>
2023-09-01 13:14:44 +0900

    (refactor) Deprecation을 사용하고 불필요한 서브 함수들 삭제

--- TDD-practice/Model/GuildedRose/GuildedRose.swift
+++ TDD-practice/Model/GuildedRose/GuildedRose.swift
@@ -65,38 +65,6 @@ public class GildedRose {
         self.items = items
     }
 
-    fileprivate func handleBackStagePass(item: Item) {
-        if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-            increaseQualityIfPossible(item)
-        }
-    }
-
-    fileprivate func increaseQualityIfPossible(_ item: Item) {
-        guard item.quality < 50 else { return }
-        let amount = increasingAmount(sellIn: item.sellIn)
-        item.quality += amount
-    }
-
-    private func increasingAmount(sellIn: Int) -> Int {
-        switch sellIn {
-        case 1...5: return 2
-        case 6...10: return 1
-        default: return 0
-        }
-    }
-
-    func shouldQualityDecrease(item: Item) -> Bool {
-        item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert" && item.name != "Sulfuras, Hand of Ragnaros"
-    }
-
-    fileprivate func shouldQualitySame(item: Item) -> Bool {
-        return item.name == "Sulfuras, Hand of Ragnaros"
-    }
-
-    fileprivate func canQualityDecrease(item: Item) -> Bool {
-        return item.quality > 0
-    }
-
     func depreciation(of item: Item) -> Depreciation {
         switch item.name {
         case "Aged Brie": return .increasing
@@ -108,20 +76,7 @@ public class GildedRose {
 
     public func updateQuality() {
         for i in 0 ..< items.count {
-            switch depreciation(of: items[i]) {
-            case .decreasing:
-                if canQualityDecrease(item: items[i]) {
-                    items[i].quality = items[i].quality - 1
-                }
-            case .increasing:
-                if items[i].quality < 50 {
-                    items[i].quality = items[i].quality + 1
-
-                    handleBackStagePass(item: items[i])
-                }
-            case .same:
-                break
-            }
+            depreciation(of: items[i]).apply(item: items[i])
 
             if items[i].name != "Sulfuras, Hand of Ragnaros" {
                 items[i].sellIn = items[i].sellIn - 1
