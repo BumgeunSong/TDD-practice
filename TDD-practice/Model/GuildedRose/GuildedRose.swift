55fd3ed57061ca039eb679a2384292e9fcd0b60f
bumgeun.song <bumgeun.song@toss.im>
2023-09-04 10:21:06 +0900

    (refactor) notEqual -> equal로 바꿔서 가독성 높이기 (2)

--- TDD-practice/Model/GuildedRose/GuildedRose.swift
+++ TDD-practice/Model/GuildedRose/GuildedRose.swift
@@ -77,16 +77,17 @@ public class GildedRose {
         }
 
         if item.sellIn < 0 {
-            if item.name != "Aged Brie" {
+            if item.name == "Aged Brie" {
+                if item.quality < 50 {
+                    item.quality = item.quality + 1
+                }
+            } else {
                 if item.name == "Backstage passes to a TAFKAL80ETC concert" {
                     item.quality = item.quality - item.quality
                 } else if item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros" {
                     item.quality = item.quality - 1
                 }
-            } else {
-                if item.quality < 50 {
-                    item.quality = item.quality + 1
-                }
+
             }
         }
     }
