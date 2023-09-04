53acc0686f21ae6e3a0e5a579d7d84b80d5485a1
bumgeun.song <bumgeun.song@toss.im>
2023-09-04 10:06:24 +0900

    (refactor) 유통기한 관련 함수를 분리

--- TDD-practice/Model/GuildedRose/GuildedRose.swift
+++ TDD-practice/Model/GuildedRose/GuildedRose.swift
@@ -71,31 +71,35 @@ public class GildedRose {
     }
 
 
+    fileprivate func handeExpiration(item: Item) {
+        if item.name != "Sulfuras, Hand of Ragnaros" {
+            item.sellIn = item.sellIn - 1
+        }
+
+        if item.sellIn < 0 {
+            if item.name != "Aged Brie" {
+                if item.name != "Backstage passes to a TAFKAL80ETC concert" {
+                    if item.quality > 0 {
+                        if item.name != "Sulfuras, Hand of Ragnaros" {
+                            item.quality = item.quality - 1
+                        }
+                    }
+                } else {
+                    item.quality = item.quality - item.quality
+                }
+            } else {
+                if item.quality < 50 {
+                    item.quality = item.quality + 1
+                }
+            }
+        }
+    }
+
     public func updateQuality() {
         for i in 0 ..< items.count {
             Depreciation.of(item: items[i]).apply(item: items[i])
 
-            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if items[i].sellIn < 0 {
-                if items[i].name != "Aged Brie" {
-                    if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].quality > 0 {
-                            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if items[i].quality < 50 {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+            handeExpiration(item: items[i])
         }
     }
 }
