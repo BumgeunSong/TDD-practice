7b70f93cf6ca7d9ed6f5ce36fb95a8513d0719ee
bumgeun.song <bumgeun.song@toss.im>
2023-09-04 10:18:49 +0900

    (refactor) if문 간단하게 합치기

--- TDD-practice/Model/GuildedRose/GuildedRose.swift
+++ TDD-practice/Model/GuildedRose/GuildedRose.swift
@@ -79,10 +79,8 @@ public class GildedRose {
         if item.sellIn < 0 {
             if item.name != "Aged Brie" {
                 if item.name != "Backstage passes to a TAFKAL80ETC concert" {
-                    if item.quality > 0 {
-                        if item.name != "Sulfuras, Hand of Ragnaros" {
-                            item.quality = item.quality - 1
-                        }
+                    if item.quality > 0 && item.name != "Sulfuras, Hand of Ragnaros" {
+                        item.quality = item.quality - 1
                     }
                 } else {
                     item.quality = item.quality - item.quality
